e1a69619a4ab58fe0e02138d5ea351bab2ad1de5
samuelbi11 <72668227+samolo-H3LL0@users.noreply.github.com>
2021-02-25 12:46:39 +0100

    creado JS

---
+++ codigo/javascript.js
@@ -0,0 +1,9 @@
+const dignidad = require('./dignidad.json');
+
+
+/* tambien puedes usar el { nivel } para importar un solo objeto. 
+si haces esto procura cambiar el console.log() de abajo a console.log(nivel) 
+y el const dignidad --> a const { nivel } = require('./dignidad.json')
+*/
+
+console.log(dignidad.nivel)
